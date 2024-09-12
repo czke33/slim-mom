00b992af20b08a23c39ba2d1560a49a9cd9b417a
czke33 <czke33@gmail.com>
2024-09-12 14:38:53 +0300

    add port 3000

--- src/pages/DiaryPage/DiaryPage.jsx
+++ src/pages/DiaryPage/DiaryPage.jsx
@@ -27,7 +27,7 @@ const DiaryPage = () => {
       const fetchConsumedProducts = async () => {
         try {
           const response = await axios.get(
-            'http://localhost:5000/api/products/day-info',
+            'http://localhost:3000/api/products/day-info',
             {
               params: { date: selectedDate.toISOString() },
               headers: {
@@ -59,7 +59,7 @@ const DiaryPage = () => {
   const handleSaveProduct = async product => {
     try {
       const response = await axios.post(
-        'http://localhost:5000/api/products/consumed',
+        'http://localhost:3000/api/products/consumed',
         {
           productId: product._id,
           date: selectedDate,
