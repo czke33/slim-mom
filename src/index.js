ae0406eb2f4993b7563771266a179f3f0209235e
czke33 <czke33@gmail.com>
2024-09-12 16:59:58 +0300

    remove basename from index

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <BloodTypeProvider>
         <CalorieInfoProvider>
           <ConsumedProductsProvider>
-            <BrowserRouter basename="/slim-mom">
+            <BrowserRouter basename="/">
               <App />
             </BrowserRouter>
           </ConsumedProductsProvider>
